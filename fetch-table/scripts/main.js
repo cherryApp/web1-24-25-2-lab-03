98dc8c75d29cc8d3467151db9c66f72957d459c8
cherryApp <j.cserko@gmail.com>
2025-04-17 17:02:08 +0200

    thead

--- fetch-table/scripts/main.js
+++ fetch-table/scripts/main.js
@@ -1,7 +1,22 @@
 import { getAll, remove } from './db.js';
 
+const table = document.querySelector('table.table');
+
 const generateTable = (tableRows = []) => {
-    console.log(tableRows);
+    const keys = Object.keys(tableRows[0]);
+
+    // Thead
+    const thead = document.createElement('thead');
+    table.appendChild(thead);
+
+    const tr = document.createElement('tr');
+    thead.appendChild(tr);
+
+    keys.forEach( key => {
+        const th = document.createElement('th');
+        tr.appendChild(th);
+        th.innerText = key;
+    });
 };
 
 getAll().then( data => generateTable(data) );
