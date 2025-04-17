11ddc8d9736f8b3aa654721d4892dda85c7ee293
cherryApp <j.cserko@gmail.com>
2025-04-17 17:26:39 +0200

    fetch done

--- fetch-table/scripts/main.js
+++ fetch-table/scripts/main.js
@@ -31,6 +31,32 @@ const generateTable = (tableRows = []) => {
             tr.appendChild(td);
             td.innerText = row[key];
         });
+
+        const td = document.createElement('td');
+        tr.appendChild(td);
+        
+        const btnGroup = document.createElement('div');
+        td.appendChild(btnGroup);
+        btnGroup.classList.add('btn-group');
+
+        const editBtn = document.createElement('button');
+        btnGroup.appendChild(editBtn);
+        editBtn.classList.add('btn', 'btn-info');
+        editBtn.innerHTML = '<i class="fa fa-pencil"></i>';
+        editBtn.addEventListener('click', () => {
+            alert( JSON.stringify(row) );
+        });
+        
+        const deleteBtn = document.createElement('button');
+        btnGroup.appendChild(deleteBtn);
+        deleteBtn.classList.add('btn', 'btn-danger');
+        deleteBtn.innerHTML = '<i class="fa fa-trash"></i>';
+        deleteBtn.addEventListener('click', async () => {
+            if ( confirm('Biztos hogy törlöd az adatsort?') ) {
+                await remove(row.id);
+                tr.parentElement.removeChild(tr);
+            }
+        });
     });
 };
 
